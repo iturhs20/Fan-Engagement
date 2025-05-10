705a9f8b10f7c54a660cd65a64550e8443c5158c
iturhs20 <155578005+CreativeTecscapes@users.noreply.github.com>
2025-05-10 21:50:34 +0530

    chart value round off

--- app/components/contest/TimeSpentBarChart.jsx
+++ app/components/contest/TimeSpentBarChart.jsx
@@ -5,19 +5,19 @@ const TimeSpentBarChart = ({ data }) => {
   const result = {};
   data.forEach(row => {
     if (!row.Contest_ID) return;
-    result[row.Contest_ID] = (result[row.Contest_ID] || 0) + parseFloat(row.Time_Spent_Hours || 0);
+    result[row.Contest_ID] = (result[row.Contest_ID] || 0) + Math.round(parseFloat(row.Time_Spent_Hours || 0));
   });
 
   const chartData = Object.entries(result).map(([key, val]) => ({ name: key, value: val }));
 
   return (
     <div className="bg-gray-900 p-4 rounded shadow mb-6">
-      <h3 className="text-lg font-semibold mb-2 text-white">Time Spent by Contest</h3>
+      <h3 className="text-lg font-semibold mb-2 text-white">Sum of Time Spent by Contest (in Hours)</h3>
       <ResponsiveContainer width="100%" height={300}>
         <BarChart data={chartData}>
           <XAxis dataKey="name" stroke="#ffffff" tick={{ fill: '#ffffff' }} />
           <YAxis stroke="#ffffff" tick={{ fill: '#ffffff' }}/>
-          <Tooltip contentStyle={{ backgroundColor: '#f9fafb', border: 'none' }}  // Tooltip value text
+          <Tooltip contentStyle={{ backgroundColor: '#f9fafb', border: 'none' }}
             labelStyle={{ color: '#000000' }}/>
           <Legend />
           <Bar dataKey="value" fill="#82ca9d" />
